647263ce5dfe04e8f7bad2c0108c6e2e951a313a
dagusen <louisaleksieb.dagusen@gmail.com>
2018-05-01 14:22:15 +0800

    Create Anime, Breed, Website views

--- louis/django/project/blogpage/views.py
+++ louis/django/project/blogpage/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, status, permissions
+
+from .models import Breed, Anime, Website
+
+from .serializers import  AnimeSerializer, BreedSerializer, WebsiteSerializer
+
 # Create your views here.
+
+class AnimeModelViewSet(viewsets.ModelViewSet):
+    queryset = Anime.objects.all()
+    serializer_class = AnimeSerializer
+    permission_classes = [permissions.AllowAny, ]
+
+class BreedModelViewSet(viewsets.ModelViewSet):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    permission_classes = [permissions.AllowAny, ]
+
+class WebsiteModelViewSet(viewsets.ModelViewSet):
+    queryset = Website.objects.all()
+    serializer_class = WebsiteSerializer
+    permission_classes = [permissions.AllowAny, ]
